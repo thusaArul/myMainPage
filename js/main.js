3cca680d4d7b729e845dda72ffef9f164283a480
thusaArul <thusa.arul@hotmail.com>
2020-08-19 12:13:41 -0400

    fixing email function

--- js/main.js
+++ js/main.js
@@ -2,34 +2,6 @@ $(document).ready(function() {
 "use strict";
 
 /*============= Counters ===========*/
-var barElements = $('.bar li');
-for (var i = 0; i < barElements.length; i++) {
- $(barElements[i]).appear(function() {
-	  var b = $(this).find("span").attr("data-width");
-	  $(this).find("span").animate({
-		width: b + "%" //The number to stop for the width of the bar.
-	  }, 1700, "easeOutCirc");
-	  $(this).find(".value").countTo({
-		from: 0, //The number to start counting from. (default: 0)
-		to: b, //The number to stop counting at. (default: 100)
-		speed: 1700, // Control speed of animation
-		refreshInterval: 40 //The number of milliseconds to wait between refreshing the counter. (default: 100)
-		});
-	});
-}
-
-var counterElements = $('.counters');
-for (var i = 0; i < counterElements.length; i++) {
-	$(counterElements[i]).appear(function() {
-	  var counter = $(this).find(".count").html();
-	  $(this).find(".count").countTo({
-		from: 0, //The number to start counting from. (default: 0)
-		to: counter, //The number to stop counting at. (default: 100)
-		speed: 1700, // Control speed of animation
-		refreshInterval: 40 //The number of milliseconds to wait between refreshing the counter. (default: 100)
-		});
-	});	
-}
 
 /*============= Bottom Handel Slide ===========*/
 $('#clientsOpen', '#clientsDropDown').on("click", function () {
@@ -72,10 +44,9 @@ function preload() {
 //-- usage --//
 //add your pics below to preload
 preload(
-    "img/blurpic.png",
-	"img/mainpic.png",
-	"img/profilepic.png",
-	"img/preloader.gif"
+    "assets/blurpic.png",
+	"assets/self.jpg",
+	"assets/self-2.jpg"
 )
 
 /*============= Touch Mobile ===========*/
